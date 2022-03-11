ef6ef1ceadf80331e9552d422c0f2e9797612124
Carol <carolnjeri32235@gmail.com>
2022-03-11 22:35:16 +0300

    hexa

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,19 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b=0; b<9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
-		}		
+			
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+			
+		}
 	}
 	putchar('\n');
 	return (0);

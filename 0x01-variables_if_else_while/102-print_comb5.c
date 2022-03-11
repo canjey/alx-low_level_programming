508aeb33a866fd6c84658516ce44c73c93eaf136
Carol <carolnjeri32235@gmail.com>
2022-03-11 22:15:08 +0300

    hexa

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,13 +19,13 @@ int main(void)
 			putchar (' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
-			
-			if (a ==98 && b ==99)
+
+			if (a == 98 && b == 99)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
-		}		
+		}
 	}
 	putchar('\n');
 	return (0);

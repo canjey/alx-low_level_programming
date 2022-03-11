3bac4ca711f1b393cc1612f4cc242efe391d84c4
Carol <carolnjeri32235@gmail.com>
2022-03-11 22:12:48 +0300

    hexa

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always (0) Success
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b=0; b<9; b++)
+		{
+		}		
+	}
+	putchar('\n');
+	return (0);
+}

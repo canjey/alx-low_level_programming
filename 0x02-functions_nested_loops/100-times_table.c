c3d76efafc5ce93ba669b43a1fd5a94c9aae114e
Carol <carolnjeri32235@gmail.com>
2022-03-15 22:46:34 +0300

    eleven

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_times_table - prints table
+*
+*/
+
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if ((n > 0) && (n < 15))
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				c = b*a;
+				if (c != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 10) + 0);
+					_putchar(( c % 10) + 0);
+				}
+			}
+		}
+	}
+}

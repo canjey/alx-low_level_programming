664c4024ee3b24115173dfd2fad7a93fdbf8e18e
Carol <carolnjeri32235@gmail.com>
2022-03-15 18:59:27 +0300

    eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,15 +14,14 @@ void print_to_98(int n)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
-			
-		}	
+		}
 	}
-	else if ( n <= 98)
+	else if (n <= 98)
 	{
-		for ( a = 98; a >= n; a-- )
+		for (a = 98; a >= n; a--)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 		}
-	}	
+	}
 }

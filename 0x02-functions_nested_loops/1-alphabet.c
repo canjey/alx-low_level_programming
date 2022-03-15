af91431350c0aa839b11a5b324081890a4049069
Carol <carolnjeri32235@gmail.com>
2022-03-15 08:37:22 +0300

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always (0) Success
+*/
+void print_alphabet(void);
+
+int main(void)
+{
+	putchar(print_alphabet());
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+}

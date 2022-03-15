211cebaefbf61eb23df6616918870fb04e9ee18c
Carol <carolnjeri32235@gmail.com>
2022-03-15 15:30:02 +0300

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,14 @@ void print_alphabet_x10(void)
 	char c;
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	i = 0;
+	while (i <= 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
+		i++;
 	}
 }

09e8195ed5e2dc8a3cc2e8ad72b0263cfea95ca2
Carol <carolnjeri32235@gmail.com>
2022-03-15 18:24:39 +0300

    nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,27 @@
 void times_table(void)
 {
 	int a;
-	int b;
-	int c;
+	int b, result;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			c = b * a;
-			_putchar(c);
+			result = b * a;
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result <= 9)
+			{
+				_putchar((result % 10) + '0');
+			}
+			if (result > 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}

ac2c704d896d2c0a5c53f0c447bea17e0580170c
Carol <carolnjeri32235@gmail.com>
2022-03-15 18:55:10 +0300

    eleven

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_to_98 - prints numbers to 98
+* @n: declared variable
+*
+*/
+void print_to_98(int n)
+{
+	int a;
+
+	if (n >= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			
+		}	
+	}
+	else if ( n <= 98)
+	{
+		for ( a = 98; a >= n; a-- )
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+		}
+	}	
+}

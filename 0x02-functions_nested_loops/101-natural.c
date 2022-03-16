6d704ff70f5afac94a4487ade85c45acba4e16e7
Carol <carolnjeri32235@gmail.com>
2022-03-16 10:50:00 +0300

    nature

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,11 +13,11 @@ int main(void)
 	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0) || ((i % 5) == 0)
+		if ((i % 3) == 0 || ((i % 5) == 0)
 		{
 			sum += i;
 		}
-		printf("%d\n", sum)
+		printf("%d\n", sum);
 	}
 	return (0);
 }

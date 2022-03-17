bd4014f50a2ea6c4d467426d734c1d86cbf2fbf0
Carol <carolnjeri32235@gmail.com>
2022-03-17 22:57:46 +0300

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point for the function
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}

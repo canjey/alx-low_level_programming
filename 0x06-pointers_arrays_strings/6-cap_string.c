c17c73eba580269ffe9083758c7cf93cf08511e5
Carol <carolnjeri32235@gmail.com>
2022-03-24 09:07:43 +0300

    capitalise

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,10 +15,11 @@ char *cap_string(char *str)
 		{
 			str[0] = str[0] - 32;
 		}
-		if ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.') && str[i+1] >= 97 && str[i+1] <= 122)
-		{
-			str[i+1] = str[i+1] - 32;
-		}
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.')
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
 		i++;
 	}
 	return (str);

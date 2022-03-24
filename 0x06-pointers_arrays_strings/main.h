66bd80771f2438effe82d25a56788a3415b65199
Carol <carolnjeri32235@gmail.com>
2022-03-24 22:57:21 +0300

    concatenate

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,6 @@ void reverse_array(int *a, int n);
 char *leet(char *str);
 char *string_toupper(char *);
 char *cap_string(char *str);
+char *rot13(char *);
+void print_number(int n);
+void print_line(char *c, int s, int l)

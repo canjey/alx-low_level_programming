c2c6bad2b6ab49d51d4df32bfe20e746b72c7376
Carol <carolnjeri32235@gmail.com>
2022-03-28 21:11:31 +0300

    more pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,38 @@
 
 /**
-* _strstr - A function to return the first ocurrence of a string
-* @haystack: Scan the string
-* @needle: Looks for the particular string
-* Return: A pointer to a character
+*   _strstr - locate and return pointer to first occurence of substring
+*    @haystack: string to search
+*     @needle: target substring to search for
+*  Return: pointer to index of string at first occurence of whole substring
 */
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+int i = 0, j, x;
 
-	i = 0;
-	if (needle[0] == '\0')
-	{
-		return (haystack);
-	}
-	while (haystack[i] != '\0')
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i] == needle[j])
-			{
-				i++;
-			}
-			else
-			{
-				break;
-			}
-		}
-		i++;
-	}
-	return (&needle[j]);
+if (needle[0] == '\0')
+return (haystack);
+
+while (haystack[i] != '\0') /* iterate through haystack */
+{
+/* if a byte matches first char of needle */
+/* interate through needle until match ends */
+if (haystack[i] == needle[0])
+{
+x = i, j = 0;
+while (needle[j] != '\0')
+{
+if (haystack[x] == needle[j])
+x++, j++;
+else
+			break;
+} /* if matched throughout, return haystack */
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+}
+i++;
+}
+return (0); /* No match */
 }

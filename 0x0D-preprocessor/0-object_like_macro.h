e952a8cb5367cdaace3c4444fde0801e5fa8afa4
Carol <carolnjeri32235@gmail.com>
2022-04-11 11:34:01 +0300

    macros

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+/**
+* create a header file that defines a macro named size
+* as an abbreviation for token 1024
+*/
+
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+#define size 1024
+#endif

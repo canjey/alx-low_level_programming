c86df2ab58df8031e944aeee8bcc647044a7adbe
Carol <carolnjeri32235@gmail.com>
2022-04-22 21:25:34 +0300

    linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+* _strlen - returns the length of the string
+* @s: string s
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+	{
+		s++;
+	}
+	return (s - p);
+}
+
+/**
+* add_node - adds a node at the beginning of a list_t list
+* @head: reference to head of linked list
+* @str: string to be added to the new node
+* Return: address of new head
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	char *newStr;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	newStr = strdup(str);
+	if (!newStr)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->len = _strlen(newStr);
+	newNode->str = newStr;
+	newNode->next = *head;
+	*head = newNode;
+	return (*head);
+}
